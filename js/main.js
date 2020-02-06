fba58e32342995190893916f0c8f917f5bbd1c8b
Kyle Roebling <kroebling@outlook.com>
2020-02-05 19:34:21 -0600

    Added geojson data to map

--- js/main.js
+++ js/main.js
@@ -1,196 +1,57 @@
-var freeBus = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [-105.00341892242432, 39.75383843460583],
-                    [-105.0008225440979, 39.751891803969535]
-                ]
-            },
-            "properties": {
-                "popupContent": "This is a free bus line that will take you across downtown.",
-                "underConstruction": false
-            },
-            "id": 1
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [-105.0008225440979, 39.751891803969535],
-                    [-104.99820470809937, 39.74979664004068]
-                ]
-            },
-            "properties": {
-                "popupContent": "This is a free bus line that will take you across downtown.",
-                "underConstruction": true
-            },
-            "id": 2
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [-104.99820470809937, 39.74979664004068],
-                    [-104.98689651489258, 39.741052354709055]
-                ]
-            },
-            "properties": {
-                "popupContent": "This is a free bus line that will take you across downtown.",
-                "underConstruction": false
-            },
-            "id": 3
+/*
+Tornadoes by Month for the 48 States
+Author: Kyle Roebling
+Date: 2/5/2020
+This project takes monthly averages for tornadoes each state from the years 1989 to 2011 using
+leaftet mapping api
+*/
+
+// Global map variables
+var center = [38.50,-98.00]
+var tileLayer = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+
+// Function to create map object, tile map background and load data to map
+function createMap(){ 
+    
+    // Create an instance of the leaflet mapping object
+    var map = L.map('mapid',{
+        center: center,
+        zoom: 4
+    });
+    
+    //add base tilelayer
+    L.tileLayer(tileLayer, {
+        attribution: attribution
+    }).addTo(map);
+    
+    //load data to the map
+    getData(map);
+
+};
+
+
+
+
+// load dataset tornadoData.geojason using Ajax call
+function getData(map){
+    $.ajax("data/tornadoData.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response).addTo(map);
         }
-    ]
-};
-
-var myStyle = {
-    "color": "red",
-    "weight": 5,
-    "opacity": 0.65
-};
-
-var bicycleRental = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9998241,
-                    39.7471494
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 51
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9983545,
-                    39.7502833
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 52
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9963919,
-                    39.7444271
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 54
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9960754,
-                    39.7498956
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 55
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9933717,
-                    39.7477264
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 57
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9913392,
-                    39.7432392
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 58
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -104.9788452,
-                    39.6933755
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-            },
-            "id": 74
-        }
-    ]
-};
-
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-var mymap = L.map('mapid').setView([39.75621, -104.99404], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1Ijoia3JvZWJsaW5nIiwiYSI6ImNqeXczaGplMjB3YjgzYmxyZGU1OG90bXUifQ.ItIrq8YGHvZIilkcx-U8Ag'
-}).addTo(mymap);
-
-function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
+    });
 }
 
-L.geoJSON(freeBus,{
-    style: myStyle,
-    onEachFeature: onEachFeature
-}).addTo(mymap);
 
-L.geoJSON(bicycleRental,{
-    pointToLayer: function(feature, latlng){
-        return L.circleMarker(latlng, geojsonMarkerOptions)
-            }
-}).addTo(mymap);
+
+
+
+
+
+
+
+
+
+$(document).ready(createMap);
