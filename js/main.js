f187ea8af840ed86b7d3e96abbc23b89f918bafb
Kyle Roebling <kroebling@outlook.com>
2020-03-01 18:50:58 -0600

    added comments

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
 /*
-Tornadoes by Month for the 48 States
+Average Tornadoes by Month for the 48 States
 Author: Kyle Roebling
 Date: 2/5/2020
 This project takes monthly averages for tornadoes each state from the years 1989 to 2011 using
-leaftet mapping api
+leaftet mapping api.
+
+1). Call createMap()
+2). Call getData()
+3). Download the two geoJSON files
+4). After ajax download is complete create the geoJSON layers for leaflet
+5). Create Sequence Contols for map
+6). Create map controls for map
+7). Check the intial radio button status to set map style
+8). Create legend based off the radion buttom status
+9). Event handlers wait for changes in radio button and monthly data from slider and arrows
+
+
 */
 
 // Global map variables
 var center = [38.50,-98.00]
 //var tileLayer = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 var tileLayer = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3JvZWJsaW5nIiwiYSI6ImNqeXczaGplMjB3YjgzYmxyZGU1OG90bXUifQ.ItIrq8YGHvZIilkcx-U8Ag'
-
 var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 index = 0
 var point = true;
 var polygon = false;
+
 
 
 // Function to create map object, tile map background and load data to map
@@ -42,35 +54,39 @@ function createMap(){
     }).addTo(map);
     
     
-    
     //load data from source files
     getData(map);
     
 };
 
-//calculate the radius of each proportional symbol
+//function to calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
+    
     //scale factor to adjust symbol size evenly
     var scaleFactor = 50;
+    
     //area based on attribute value and scale factor
     var area = attValue * scaleFactor;
+    
     //radius calculated based on area
     var radius = Math.sqrt(area/Math.PI);
+    
     return radius;
 };
 
 //Calculate the max, mean, and min values for a given attribute
 function getCircleValues(map,month){
-    //start with min at highest possible and max at lowest possible number
+    
+    //start with max at lowest possible number and min will always be 1 in this dataset
     var min = 1,
         max = -Infinity;
-
+    
+    //iterate through each layer in the map
     map.eachLayer(function(layer){
 
         //get the attribute value
         if (layer.feature){
             var attributeValue = Number(layer.feature.properties[month]);
-
 
             //test for max
             if (attributeValue > max){
@@ -134,13 +150,14 @@ function pointToLayer(feature,latlng, months){
         }
     });
     
+    //call function panel_list to add feature attribute to the list
     panel_list(feature,attribute);
         
-
     //return the circle marker to the L.geoJson pointToLayer option
     return layer;
 };
 
+//create the proportional symbol points geoJSON
 function createPoints(map,data, months){
     //create a Leaflet GeoJSON layer and add it to the map
     return  L.geoJson(data, {
@@ -152,6 +169,7 @@ function createPoints(map,data, months){
                 });
 }
 
+// Create radio buttons that control whether the maps displays proportional or choropleth
 function mapControls(map,months){
     
     var SequenceControl = L.Control.extend({
@@ -181,6 +199,7 @@ function mapControls(map,months){
     
 }
 
+//function that creates the slider and arrow controls to sequence the map through the months
 function createSequenceControls(map,months){
     var SequenceControl = L.Control.extend({
         options:{
@@ -256,6 +275,7 @@ function createSequenceControls(map,months){
     });  
 }
 
+//function that creates the divs for the legend
 function createLegend(map,months){
 
     var LegendControl = L.Control.extend({
@@ -282,6 +302,7 @@ function createLegend(map,months){
     
 }
 
+//function that shows the month in the main header
 function createHeader(){
     
     $('#header_month').empty();
@@ -290,7 +311,7 @@ function createHeader(){
 }
 
 
-
+//function that creates the dynamic choropleth legend
 function legendChoropleth(month){
     
     $('#month-legend').html(month);
@@ -304,11 +325,13 @@ function legendChoropleth(month){
     };
 }
 
+//function that creates the dynamic proprotional symbol legend
 function legendSymbol(map,month){
     
+        //clear the div of all contents
         $('#temporal-legend').empty();
     
-        //Step 1: start attribute legend svg string
+        //start attribute legend svg string
         var svg = '<svg id="attribute-legend" width="180px" height="60px">';
         
         //object to base loop on
@@ -336,6 +359,7 @@ function legendSymbol(map,month){
         updateLegend(map,month);
 }
 
+//function that updates the proportional symbols size in the legend
 function updateLegend(map,month){
    
     $('#month-legend').html(month);
@@ -358,11 +382,12 @@ function updateLegend(map,month){
     }
 };
 
-                            
+//fucntion to get the current range slider index value                          
 function getIndex(){
     return $('.range-slider').val();
 }
 
+//function to add list items to the panel 
 function panel_list(feature,month){
     
     if (feature.properties[month] > 0){
@@ -372,11 +397,12 @@ function panel_list(feature,month){
     };
 };
 
+//function to clear all contents from the panel div
 function clearPanel(){
     $("ul").empty();
 };
 
-// Update the symbols as the months change
+// Update the symbols in the map as the months change
 function updatePoints(map, month){
 
     map.eachLayer(function(layer){
@@ -389,6 +415,7 @@ function updatePoints(map, month){
             //For each feature, determine its value for the selected attribute
             var newValue = Number(props[month]);
             
+            //update the data to the new month for points only
             if (layer.feature.geometry.type === "Point"){
                 //update each feature's radius based on new attribute values
                 var radius = calcPropRadius(newValue);
@@ -396,6 +423,7 @@ function updatePoints(map, month){
               
             };
             
+            //update the data to the new month for polygons only
             if (layer.feature.geometry.type === "MultiPolygon"){
                 value = color(props[month]);
                 layer.setStyle({fillColor: value})
@@ -424,23 +452,24 @@ function updatePoints(map, month){
         };
          
     });
-    console.log(point);
-    console.log(polygon);
-
+    //if proportional symbol create the proportional symbol legend
     if(point){
          updateLegend(map,month);
+    //if polygon create the choropleth legend
     } else if (polygon){
          legendChoropleth(month);
     };
 
 }
 
+//create the polygon geoJSON
 function createPolygons(map,data){
     return L.geoJSON(data,{
         style: choropleth,
     });
 }
 
+//create the colors for the choropleth map
 function color(data){
     return data > 14  ? '#f03b20' :
            data > 7   ? '#feb24c' :
@@ -448,6 +477,7 @@ function color(data){
                         '#e0e0d1';
 };
 
+//create the style for the polygon choropleth
 function choropleth(feature) {
 
     return {
@@ -460,7 +490,7 @@ function choropleth(feature) {
     };
 }
 
-
+//function that checks which radio button is checked to change map style
 function checkRadio(map,statePoints,statePolygons,month){
     // add points to map first
     statePoints.addTo(map);
@@ -519,13 +549,13 @@ function getData(map){
             createLegend(map,months);       
     });
 
-
+    //load geoJSON point data
     function pointData(){
         return $.ajax("data/tornadoData.geojson", {    
                 dataType: "json"  
             })
     };
-
+    //load geoJSON polygon data
     function polygonData(){
         return $.ajax("data/Polygon_Tornado.geojson", {
             dataType: "json"
